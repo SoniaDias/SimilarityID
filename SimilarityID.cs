4f94a5e5bfa565ce3d63cfae4f249185e8570fe6
SoniaDias <sonia.c.a.dias@gmail.com>
2021-03-03 11:35:17 +0000

    Create SimilarityID.cs

---
+++ SimilarityID.cs
@@ -0,0 +1,26 @@
+namespace SimIDExample
+{
+    public partial class index : System.Web.UI.Page
+    {
+        private void doUpdate(String query)
+        {
+            var conn_string = "some_connection_string";
+            using (SqlConnection conn = new SqlConnection(conn_string))
+            {
+                SqlCommand command = new SqlCommand(query, conn);
+                conn.Open();
+                command.ExecuteNonQuery();
+            }//end using
+        }//end doUpdate
+ 
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            doUpdate(getQuery());
+        }//end submit_Click
+ 
+        private String getQuery()
+        {
+            return "UPDATE Users SET Name = '" + name.Text + "' WHERE UID = '" + ViewState["UID"].ToString() + "'";
+        }//end getQuery
+    }//end class
+}//end namespace
